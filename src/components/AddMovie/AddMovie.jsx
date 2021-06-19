e8a23b9c66ae0cc267619aa2dc209dbcc89ad936
Ashton Gasser <ashtongasser@gmail.com>
2021-06-19 17:48:39 -0500

    inputs created

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,54 +3,81 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 
+function AddMovie() {
+  const history = useHistory;
+  const dispatch = useDispatch();
 
-
-function AddMovie(){
-const history = useHistory
-const dispatch = useDispatch();
-
-
-const [newMovie, setNewMovie] = useState({
-    title: '',
-    poster: '', 
-    description: '',
+  const [newMovie, setNewMovie] = useState({
+    title: "",
+    poster: "",
+    description: "",
     movie_id: null,
-    genre_id: null
+    genre_id: null,
+  });
 
-})
+  // useEffect(() => {
+  //     dispatch({type: 'SET_GENRES'})
+  // }, []);
 
-// useEffect(() => {
-//     dispatch({type: 'SET_GENRES'})
-// }, []);
+  const handleClick = () => {
+    // checking to see what is logged⬇
+    console.log("new movie!", newMovie);
+    // dispatch the new movie to redux⬇
+    dispatch({
+      type: "POST_MOVIES",
+      payload: newMovie,
+    });
+  }; //end handleClick
 
-const handleClick =() => {
-// checking to see what is logged⬇
-console.log('new movie!', newMovie);
-// dispatch the new movie to redux⬇
-dispatch({
-    type: 'POST_MOVIES', 
-    payload: newMovie 
-})
+  const handleInputs = (key, value) => {
+    setNewMovie({ ...newMovie, [key]: value });
+  }; //end handleInputs
 
-}//end handleClick
+  const handleBack = () => {
+    console.log("clicked cancel");
+    history.push("/");
+  }; // end handleBack
 
-const handleInputs = (key,value) => {
-    setNewMovie({...newMovie,
-        [key]: value, 
-})
-}//end handleInputs
-
-
-const handleBack = () => {
-        console.log("clicked cancel");
-        history.push('/')
-      };// end handleBack
-
-
-
-    
-
-
+  return (
+    <div>
+      <h1>Add Movie</h1>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Movie Title"
+            onChange={(event) => handleInputs("title", event.target.value)}
+          />
+          </div>
+          <div>
+              <input
+              type ="text"
+              placeholder ='Poster URL'
+              onChange={(event) => handleInputs("poster", event.target.value)}
+              />
+              </div>
+              <div>
+                  <textarea
+                  placeholder ="Movie Synopsis"
+                  rows ="6"
+                  cols="50"
+                  onChange={(event) => handleInputs('description', event.target.value)}
+                  />
+                  </div>
+                  <div>
+                      <select
+                          name="Genre"
+                          onChange={(event) => handleInputs('genre_id', event.target.value)}
+                          >
+                              
+                      </select>
+                  </div>
+                  
+              
+        
+      </form>
+    </div>
+  );
 }
 
-export default AddMovie
+export default AddMovie;
